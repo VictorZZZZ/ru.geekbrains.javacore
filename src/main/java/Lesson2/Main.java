781612c96fac6161ea011e0d4d34b8e4d030e1fc
Ka1sh1 <sonicla@gmail.com>
2020-05-28 23:57:12 +0300

    test commit with wrong code

--- src/main/java/Lesson2/Main.java
+++ src/main/java/Lesson2/Main.java
@@ -11,6 +11,51 @@ public class Main {
 //        6. ** Написать метод, в который передается не пустой одномерный целочисленный массив, метод должен вернуть true, если в массиве есть место, в котором сумма левой и правой части массива равны. Примеры: checkBalance([2, 2, 2, 1, 2, 2, || 10, 1]) → true, checkBalance([1, 1, 1, || 2, 1]) → true, граница показана символами ||, эти символы в массив не входят.
 //        7. **** Написать метод, которому на вход подается одномерный массив и число n (может быть положительным, или отрицательным), при этом метод должен сместить все элементымассива на n позиций. Для усложнения задачи нельзя пользоваться вспомогательными массивами.
 
+        private static void compare(int x) {
+            if (x >= 0) {
+                System.out.println("Value is positive");
+            } else {
+                System.out.println("Value is negative");
+            }
+        }
 
+        private static boolean checkPositive(int x){
+            if (x < 0) {
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+        private static void yoName(String x){
+
+            System.out.println("Hello " + x + " !");
+
+        }
+
+        public static void shame(int y){
+            int theYear;
+            theYear = y;
+
+            if (theYear < 100) {
+                if (theYear > 40) {
+                    theYear = theYear + 1900;
+                } else {
+                    theYear = theYear + 2000;
+                }
+            }
+
+            if (theYear % 4 == 0) {
+                if (theYear % 100 != 0) {
+                    System.out.println("IT IS A LEAP YEAR");
+                } else if (theYear % 400 == 0) {
+                    System.out.println("IT IS A LEAP YEAR");
+                } else {
+                    System.out.println("IT IS NOT A LEAP YEAR");
+                }
+            } else {
+                System.out.println("IT IS NOT A LEAP YEAR");
+            }
+        }
     }
 }
